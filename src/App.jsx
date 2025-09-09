51d18c0760c7f35a31ac833876032cafec02cc1b
HasanAlsaafen <hasansaafen1234@gmail.com>
2025-09-09 15:54:35 +0300

    Add searchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import SideBar from "./components/SideBar";
 import "./App.css";
 import Main from "./components/Main";
+import Other from "./components/Other";
 export default function App() {
   return (
     <div className="App">
       <SideBar />
       <Main />
+      <Other />
     </div>
   );
 }
